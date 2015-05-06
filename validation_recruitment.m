a2d80af6dba51ece226195dde1e45522885081fa
penguinscontrol <penguinscontrol@gmail.com>
2015-05-06 14:24:43 -0400

    validation recruitment

--- validation_recruitment.m
+++ validation_recruitment.m
@@ -1,27 +1,39 @@
 clear;clc; close all;
 
-load(['A:\CSN - Corticospinal Neuroprosthetics\Raw_data&Processed_activations\' ...
-    'Q21_6-02_trial10_recruitcurves_data.mat']);
+fold_name = 'A:\CSN - Corticospinal Neuroprosthetics\Raw_data&Processed_activations\';
+
+files_to_load = {[ fold_name ...
+    'Q21_6-02_trial01_recruitcurves_data.mat'],...
+    [fold_name ...
+    'Q21_6-02_trial09_recruitcurves_data.mat'],...
+    [fold_name ...
+    'Q21_6-02_trial10_recruitcurves_data.mat']};
 
 EMG_amp = zeros(4,8);
-which_muscle = 8;
-count = 1;
+total_EMG = [];
 
-for a = 1:8
-    
-    for b = 1:4
+which_muscle = 8;
+
+for c = 1:length(files_to_load)
+    load(files_to_load{c});
+    count = 1;
+    for a = 1:8
         
-        EMG_trace = info.windoweddata{which_muscle};
-        EMG_time = info.windowedtime{which_muscle};
-        EMG_amp(b,a) = trapz(EMG_time(count,:),abs(EMG_trace(count,:)));
-        count = count + 1;
+        for b = 1:4
+            
+            EMG_trace = info.windoweddata{which_muscle};
+            EMG_time = info.windowedtime{which_muscle};
+            EMG_amp(b,a) = trapz(EMG_time(count,:),abs(EMG_trace(count,:)));
+            count = count + 1;
+        end
+        
     end
-    
+    total_EMG = [total_EMG ; EMG_amp];
 end
 
-EMG_amp = 100 .* EMG_amp ./ (max(max(EMG_amp)));
-EMG_mean = mean(EMG_amp);
-EMG_std = std(EMG_amp);
+total_EMG = 100 .* total_EMG ./ (max(max(total_EMG)));
+EMG_mean = mean(total_EMG);
+EMG_std = std(total_EMG);
 
 bound_up = EMG_mean + EMG_std;
 bound_down = EMG_mean - EMG_std;
@@ -29,7 +41,7 @@ bound_down = EMG_mean - EMG_std;
 patch_x = [stimvalues fliplr(stimvalues)];
 patch_y = [bound_up fliplr(bound_down)];
 
-h1 = plot(stimvalues, EMG_amp, 'k.','DisplayName','Data');
+h1 = plot(stimvalues, total_EMG, 'k.','DisplayName','Data');
 hold on;
 h2 = plot(stimvalues,EMG_mean,'k-','DisplayName','Mean');
 h3 = patch(patch_x,patch_y,'r','EdgeColor','none','FaceAlpha',0.5,'DisplayName','Standard Deviation');
